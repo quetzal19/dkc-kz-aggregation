69d0127304b3d0a4c152dfafae341a8a934472c8
Sergey Latyshev <latyshev@panfilov.digital>
2024-07-19 13:06:16 +0300

    feat: Added logging for products import handler
    https://track.ratio.digital/youtrack/issue/DKCKZ-348

--- backend/src/MessageHandler/ProductImportHandler.php
+++ backend/src/MessageHandler/ProductImportHandler.php
@@ -6,13 +6,17 @@ use App\Dto\Product;
 use App\Message\ProductImport;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[AsMessageHandler]
 class ProductImportHandler
 {
+    protected const DATA_FORMAT = 'json';
+
     public function __construct(
-//        protected SerializerInterface $serializer,
-//        protected ValidatorInterface $validator,
+        protected SerializerInterface $serializer,
+        protected ValidatorInterface $validator,
         protected LoggerInterface $productImportLogger
     ) {
     }
@@ -22,13 +26,14 @@ class ProductImportHandler
         $jsonData = $message->getMessage();
 
         // Deserialize JSON to object
-        $myData = $this->serializer->deserialize($jsonData, Product::class, 'json');
+        $myData = $this->serializer->deserialize($jsonData, Product::class, self::DATA_FORMAT);
 
         // Validate the object
         $errors = $this->validator->validate($myData);
 
         if (count($errors) > 0) {
             // log errors
+            $this->productImportLogger->error($errors);
         }
     }
 }
