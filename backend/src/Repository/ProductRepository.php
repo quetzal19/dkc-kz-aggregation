08cce52172db41d18d0a4605641a30fd50dd8916
Sergey Latyshev <latyshev@panfilov.digital>
2024-07-24 10:43:44 +0300

    feat: Added base ProductListService upgrade
    https://track.ratio.digital/youtrack/issue/DKCKZ-357

--- backend/src/Repository/ProductRepository.php
+++ backend/src/Repository/ProductRepository.php
@@ -3,8 +3,11 @@
 namespace App\Repository;
 
 use App\Document\Product;
+use App\Enum\SortOrder;
+use App\Enum\SortType;
 use Doctrine\Bundle\MongoDBBundle\ManagerRegistry;
 use Doctrine\Bundle\MongoDBBundle\Repository\ServiceDocumentRepository;
+use Doctrine\ODM\MongoDB\MongoDBException;
 
 /**
  * Class ProductRepository
@@ -21,5 +24,38 @@ class ProductRepository extends ServiceDocumentRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * @throws MongoDBException
+     */
+    public function findBySectionAndFilters(
+        string $sectionCode,
+        array $filters,
+        SortType $sortType,
+        SortOrder $sortOrder,
+        int $page,
+        int $ipp
+    ): array {
+
+        $query = $this->createQueryBuilder()
+            ->field('sectionCode')->equals($sectionCode)
+            ->field('active')->equals(true);
+
+        if ($sortType === SortType::CODE) {
+            $query->sort('code', $sortOrder->getIntValue());
+        } elseif ($sortType === SortType::NAME) {
+            $query->sort('name', $sortOrder->getIntValue());
+        } elseif ($sortType === SortType::WEIGHT) {
+            $query->sort('weight', $sortOrder->getIntValue());
+        } elseif ($sortType === SortType::VOLUME) {
+            $query->sort('volume', $sortOrder->getIntValue());
+        }
+
+        $query
+            ->skip(($page - 1) * $ipp)
+            ->limit($ipp);
+
+        return $query->getQuery()->execute();
     }
 }
