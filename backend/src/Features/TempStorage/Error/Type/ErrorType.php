0da095664be7bde62b0d6e42ce1d0fd59c0c3880
figurecode <figurecode@gmail.com>
2024-10-18 18:07:05 +0300

    feat: добавлен новый тип ошибок для дубликатов Аналогов и Аксессуаров
    https://track.ratio.digital/youtrack/issue/DKCKZ-471

--- backend/src/Features/TempStorage/Error/Type/ErrorType.php
+++ backend/src/Features/TempStorage/Error/Type/ErrorType.php
@@ -7,6 +7,7 @@ enum ErrorType: string
     case DATA_NOT_READY = 'DATA_NOT_READY';
     case VALIDATION_ERROR = 'VALIDATION_ERROR';
     case ENTITY_ALREADY_EXISTS = 'ENTITY_ALREADY_EXISTS';
+    case DUPLICATE = 'DUPLICATE';
     case ENTITY_NOT_FOUND = 'ENTITY_NOT_FOUND';
     case UNKNOWN_ERROR = 'UNKNOWN_ERROR';
 
@@ -17,6 +18,7 @@ enum ErrorType: string
             self::ENTITY_NOT_FOUND,
             self::ENTITY_ALREADY_EXISTS,
             self::UNKNOWN_ERROR,
+            self::DUPLICATE,
         ];
     }
 }
